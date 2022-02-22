fe9680f044dbde2751ec3232c7ebea13163ed4ae
Gyeonghun Park <zrhdnl@gmail.com>
2022-02-22 14:27:59 +0900

    Add Navbar

--- components/layouts/main.jsx
+++ components/layouts/main.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { Box, Container } from "@chakra-ui/react";
+import { Navbar } from "@components/ui";
 
 function MainLayout({ children, router }) {
   return (
@@ -8,8 +9,15 @@ function MainLayout({ children, router }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Gyeonghun's blog" />
         <meta name="author" content="Gyeonghun Park" />
+        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <meta property="og:site_name" content="Gyeonghun Park's Blog" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/card.png" />
         <title>Gyeonghun Park - Blog</title>
       </Head>
+
+      <Navbar path={router.asPath} />
 
       <Container maxW="container.md" pt={14}>
         {children}
--- components/ui/index.js
+++ components/ui/index.js
@@ -1,1 +1,4 @@
 export { default as Fonts } from "./fonts";
+export { default as Logo } from "./logo";
+export { default as Navbar } from "./navbar";
+export { default as ThemeToggleButton } from "./themeToggleButton";
